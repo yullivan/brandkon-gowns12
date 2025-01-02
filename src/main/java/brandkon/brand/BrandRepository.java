327029a634b1e71f2816bcd0b50474f99246a21c
정해준 <wjdgowns4414@gmail.com>
2025-01-02 13:52:38 +0900

    브랜드 Dto,Entity,Repository 생성

---
+++ src/main/java/brandkon/brand/BrandRepository.java
@@ -0,0 +1,8 @@
+package brandkon.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand,Long> {
+}
